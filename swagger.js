91866bf91454da355f97a10b3469a2dde09d57ec
hexschool.north.13.horae <hexschool.north.13.horae@gmail.com>
2023-05-28 11:53:02 +0800

    [feat] 調整swagger.js參數

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
       "Horae是希臘神話中掌管時間和秩序的女神，把掌握時間這個精神作為主題的看板生產力工具。提供團隊協作功能，並結合計時、番茄鐘等專注工具。致力為團隊累積進度，穩健邁向專案目標。",
   },
   host: process.env.BACK_END,
-  schemes: ["http", "https"],
+  schemes: ["https", "http"],
   tags: [
     { name: "User", description: "使用者相關" },
     { name: "WorkSpace", description: "工作區相關" },
